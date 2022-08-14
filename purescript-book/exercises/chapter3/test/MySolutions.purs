1609420596a579e6b1866598b277e59adc0521ca
Anthony Suárez <neolight1010@gmail.com>
2022-08-14 14:31:29 -0500

    Complete chapter 3 exercises

--- purescript-book/exercises/chapter3/test/MySolutions.purs
+++ purescript-book/exercises/chapter3/test/MySolutions.purs
@@ -2,4 +2,27 @@ module Test.MySolutions where
 
 import Prelude
 
--- Note to reader: Add your solutions to this file
+import Data.AddressBook (Entry, AddressBook)
+import Data.List (filter, head, nubByEq, null)
+import Data.Maybe (Maybe)
+
+findEntryByStreet :: String -> AddressBook -> Maybe Entry
+findEntryByStreet street = head <<< filter filterEntry
+  where
+    filterEntry :: Entry -> Boolean
+    filterEntry = _.address.street >>> isCorrectStreet
+
+    isCorrectStreet :: String -> Boolean
+    isCorrectStreet = (==) street
+
+isInBook :: String -> String -> AddressBook -> Boolean
+isInBook firstName lastName = not null <<< filter filterEntry
+  where
+    filterEntry :: Entry -> Boolean
+    filterEntry entry = entry.firstName == firstName && entry.lastName == lastName
+
+removeDuplicates :: AddressBook -> AddressBook
+removeDuplicates = nubByEq eqEntries
+  where
+    eqEntries :: Entry -> Entry -> Boolean
+    eqEntries e1 e2 = e1.firstName == e2.firstName && e1.lastName == e2.lastName
