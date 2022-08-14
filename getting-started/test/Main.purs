8302a4d6416b8dc859e2f91e1b251b1494f85801
Anthony Suárez <neolight1010@gmail.com>
2022-08-14 11:54:33 -0500

    Complete Getting Started guide

---
+++ getting-started/test/Main.purs
@@ -0,0 +1,13 @@
+module Test.Main where
+
+import Prelude
+
+import Effect (Effect)
+
+import Euler (answer)
+
+import Test.Assert (assert)
+
+main :: Effect Unit
+main = do
+  assert (answer == 233168)
