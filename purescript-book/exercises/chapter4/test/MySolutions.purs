c4c59622057cc9fcc1161ded4850fc407f0de2c6
Anthony Suárez <neolight1010@gmail.com>
2022-08-17 19:06:13 -0500

    Advance chapter 4 exercises

--- purescript-book/exercises/chapter4/test/MySolutions.purs
+++ purescript-book/exercises/chapter4/test/MySolutions.purs
@@ -2,4 +2,27 @@ module Test.MySolutions where
 
 import Prelude
 
--- Note to reader: Add your solutions to this file
+import Data.Array (head, null, tail)
+import Data.Maybe (fromMaybe)
+
+isEven :: Int -> Boolean
+isEven n =
+  if n == 2 || n == 0 then
+    true
+  else if n == 1 then
+    false
+  else
+    if n < 0 then
+      isEven $ n + 2
+    else
+      isEven $ n - 2
+
+countEven :: Array Int -> Int
+countEven xs =
+  if null xs then
+    0
+  else
+    oneIfEven (fromMaybe 1 $ head xs) + countEven (fromMaybe [] $ tail xs)
+
+  where
+    oneIfEven n = if mod n 2 == 0 then 1 else 0
